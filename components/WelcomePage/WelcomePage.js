349e9d82aa49da3f853524d0671ae077eeaa377e
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-17 16:33:50 -0700

    Button, welcome message, and alert onPress of button in WelcomePage component
    Co-authored-by: Pol Sieira <polasp17@gmail.com>

--- components/WelcomePage/WelcomePage.js
+++ components/WelcomePage/WelcomePage.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function WelcomePage() {
   return (
     <View style={styles.container}>
+      <LinearGradient
+        colors={['#004EFF', '#88CCF1']}
+        style={styles.header}
+      ></LinearGradient>
       <Image
         source={require('../../assets/images/doctor.png')}
         style={styles.icon}
       />
-      <LinearGradient
-        colors={['#004EFF', '#FFF']}
-        style={styles.header}
-      ></LinearGradient>
+      <View style={styles.welcome}>
+        <Text style={styles.greeting}>HELLO!</Text>
+        <Text style={styles.message}>
+          Welcome to MeMD, an online platform designed to help diagnose symptoms
+          and locate potential doctors. Please click BEGIN to start your health
+          checkup.
+        </Text>
+        <Button
+          title='BEGIN'
+          color='#8CDEDC'
+          onPress={() => Alert.alert('Oh my GOD')}
+          // TODO: Link to Terms of Service and Privacy Policy page
+        />
+      </View>
     </View>
   );
 }
@@ -23,13 +37,33 @@ const styles = StyleSheet.create({
     flex: 1
   },
   header: {
-    flex: 0.25
+    flex: 0.2
   },
   icon: {
-    alignSelf: center,
-    borderWidth: 1,
-    borderRadius: 5,
+    alignSelf: 'center',
+    borderWidth: 0,
+    borderRadius: 50,
+    height: 200,
+    marginTop: 35,
+    width: 200
+  },
+  welcome: {
+    flex: 0.5
+  },
+  greeting: {
+    // fontFamily: 'Open Sans'
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 25
+  },
+  message: {
+    fontSize: 27,
+    margin: 25
+  },
+  button: {
+    color: '#8CDEDC',
     height: 100,
-    width: 100
+    margin: 10,
+    width: '75%'
   }
 });
