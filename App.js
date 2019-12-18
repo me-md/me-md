26cfbdaadf526d7f7f837c3a5b7547c67493a847
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-18 15:45:25 -0700

    Create Location component and build out initial structure with some basic styling

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import WelcomePage from './components/WelcomePage/WelcomePage';
 import BiologicalInformation from './components/BiologicalInformation/BiologicalInformation';
+import Location from './components/Location/Location';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -23,7 +24,8 @@ export default function App(props) {
       <View style={styles.container}>
         {/* {Platform.OS === 'ios' && <StatusBar barStyle='default' />} */}
         {/* <WelcomePage /> */}
-        <BiologicalInformation />
+        {/* <BiologicalInformation /> */}
+        <Location />
         {/* <AppNavigator /> */}
       </View>
     );
