59a193a49d4ab7c7df83a99e62ffa9b065a7591e
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-17 21:32:07 -0700

    Finesse styling within BiologicalInformation component

--- components/BiologicalInformation/BiologicalInformation.js
+++ components/BiologicalInformation/BiologicalInformation.js
@@ -8,24 +8,28 @@ export default class BiologicalInformation extends Component {
   render() {
     return (
       <View style={styles.biologicalInformationContainer}>
-        <LinearGradient colors={['#004EFF', '#88CCF1']}>
-          <Text style={styles.biologicalInformationQuestion}>
-            What is your biological sex?
-          </Text>
-          <Button>
-            <MaterialCommunityIcons
-              name='gender-female'
-              size={32}
-              color='black'
-            />
-          </Button>
-          <Button>
-            <MaterialCommunityIcons
-              name='gender-male'
-              size={32}
-              color='black'
-            />
-          </Button>
+        <LinearGradient style={styles.header} colors={['#004EFF', '#88CCF1']}>
+          <View style={styles.biologicalInformationAnswer}>
+            <Text style={styles.biologicalInformationQuestion}>
+              What is your biological sex?
+            </Text>
+            <Button iconLeft style={styles.biologicalInformationButtons}>
+              <MaterialCommunityIcons
+                name='gender-female'
+                size={64}
+                color='black'
+              />
+              <Text style={styles.biologicalInformationButtonText}>Female</Text>
+            </Button>
+            <Button iconLeft style={styles.biologicalInformationButtons}>
+              <MaterialCommunityIcons
+                name='gender-male'
+                size={64}
+                color='black'
+              />
+              <Text style={styles.biologicalInformationButtonText}>Male</Text>
+            </Button>
+          </View>
         </LinearGradient>
       </View>
     );
@@ -33,8 +37,30 @@ export default class BiologicalInformation extends Component {
 }
 
 const styles = StyleSheet.create({
+  biologicalInformationAnswer: {
+    flex: 1
+  },
+  biologicalInformationButtons: {
+    alignSelf: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.45)',
+    borderRadius: 50,
+    height: 150,
+    margin: 25,
+    padding: 20,
+    width: 275
+  },
   biologicalInformationContainer: {
     flex: 1
   },
-  biologicalInformationQuestion: {}
+  biologicalInformationQuestion: {
+    flex: 0.5,
+    fontSize: 24,
+    height: 30
+  },
+  biologicalInformationButtonText: {
+    fontSize: 36
+  },
+  header: {
+    flex: 1
+  }
 });
