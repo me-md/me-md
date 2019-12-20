ea26abc2b6e56a1b898f813b410674f2f7a9edbe
posi7790 <posi7790@colorado.edu>
2019-12-19 20:10:37 -0500

    Add navigation to SelectAge component and add dumb slider

--- screens/SelectAge/SelectAge.js
+++ screens/SelectAge/SelectAge.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Slider, StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Item, Input } from 'native-base';
 import { Entypo } from '@expo/vector-icons';
+
+const height = Dimensions.get('window').height;
 
 export default class SelectAge extends Component {
   render() {
@@ -13,7 +15,7 @@ export default class SelectAge extends Component {
           colors={['#004EFF', '#88CCF1']}
         >
           <View style={styles.contentContainer}>
-            <Entypo name='chevron-thin-up' size={36} color='black' />
+            <Entypo name='chevron-thin-up' size={36} color='black' onPress={() => this.props.navigation.navigate('TermsAndConditions')} />
             <View style={styles.selectAgeChildContainer}>
               <Text style={styles.selectAgeHeader}>Age</Text>
               <Item style={styles.selectAgeInput}>
@@ -22,6 +24,17 @@ export default class SelectAge extends Component {
                   style={styles.selectAgeInputText}
                 />
               </Item>
+              <Slider
+                minimumValue={18}
+                maximumValue={110}
+                minimumTrackTintColor="#1EB1FC"
+                maximumTractTintColor="#1EB1FC"
+                step={1}
+                value={40}
+                // onValueChange={value => this.onValueChange(value)}
+                style={styles.slider}
+                thumbTintColor="#1EB1FC"
+              />
             </View>
             <Entypo name='chevron-thin-down' size={36} color='black' />
           </View>
@@ -47,12 +60,17 @@ const styles = StyleSheet.create({
   selectAgeHeader: {
     color: '#fff',
     fontSize: 48,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    width: 200
   },
   selectAgeInput: {
     color: '#FFF'
   },
   selectAgeChildContainer: {
     flex: 0.75
+  },
+  slider: {
+    marginTop: 25,
+    width: 200
   }
 });
