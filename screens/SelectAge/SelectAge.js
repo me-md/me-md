37875adf4ca08b09365e14ace936e54cbe08e170
posi7790 <posi7790@colorado.edu>
2019-12-21 14:46:55 -0500

    Add navigation to existing screens

--- screens/SelectAge/SelectAge.js
+++ screens/SelectAge/SelectAge.js
@@ -1,41 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Item, Input } from 'native-base';
 import { Entypo } from '@expo/vector-icons';
 
-export default class SelectAge extends Component {
-  render() {
-    return (
-      <View style={styles.selectAgeContainer}>
-        <LinearGradient
-          style={styles.gradientBackground}
-          colors={['#004EFF', '#88CCF1']}
-        >
-          <View style={styles.contentContainer}>
-            <Entypo name='chevron-thin-up' size={36} color='black' />
-            <View style={styles.selectAgeChildContainer}>
-              <Text style={styles.selectAgeHeader}>Age</Text>
-              <Item style={styles.selectAgeInput}>
-                <Input
-                  placeholder='Enter your age'
-                  style={styles.selectAgeInputText}
-                />
-              </Item>
-            </View>
-            <Entypo name='chevron-thin-down' size={36} color='black' />
+export default function SelectAge({ navigation }) {
+  console.log(navigation.state.params)
+
+  const [state, setState] = useState({
+    age: '',
+    sex: navigation.state.params
+  })
+
+  return (
+    <View style={styles.selectAgeContainer} >
+      <LinearGradient
+        style={styles.gradientBackground}
+        colors={['#004EFF', '#88CCF1']}
+      >
+        <View style={styles.contentContainer}>
+          <Entypo
+            name='chevron-thin-up'
+            size={36}
+            color='white'
+            onPress={() => navigation.navigate('BiologicalInformation')}
+          />
+          <View style={styles.selectAgeChildContainer}>
+            <Text style={styles.selectAgeHeader}>Age</Text>
+            <Item style={styles.selectAgeInput}>
+              <Input
+                placeholder='Enter your age'
+                style={styles.selectAgeInputText}
+              />
+            </Item>
           </View>
-        </LinearGradient>
-      </View>
-    );
-  }
+          <Entypo name='chevron-thin-down' size={36} color='white' />
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   contentContainer: {
     alignItems: 'center',
     flex: 1,
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    marginTop: 20
   },
   selectAgeContainer: {
     flex: 1
