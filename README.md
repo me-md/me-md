2a51d2230353a7f223096807f9c1d7d66e371c66
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-06 18:15:49 -0700

    Add title, report icon, input and buttons with minimal styling

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 #### Image Credits
 
-<div>Icons made by <a href="https://www.flaticon.com/authors/popcorns-arts" title="Icon Pond">Icon Pond</a>, <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> and <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+<div>Icons made by <a href="https://www.flaticon.com/authors/popcorns-arts" title="Icon Pond">Icon Pond</a>, <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a>, <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> and <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 >>>>>>> Add icon for terms and conditions
--- screens/EmailScreen/EmailScreen.js
+++ screens/EmailScreen/EmailScreen.js
@@ -1,20 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dimensions, Image, StyleSheet, View } from 'react-native';
-import { Button, Text } from 'native-base';
+import { Button, Input, Item, Text } from 'native-base';
 import { Header } from '../../components/Header';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const height = Dimensions.get('window').height;
 
 export default function EmailScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+
   return (
     <View style={styles.container}>
       <Header />
+      <Text style={styles.title}>Send Detailed Report:</Text>
+      <Image
+        source={require('../../assets/images/report.png')}
+        style={styles.report}
+      />
+      <Item style={styles.input}>
+        <Input
+          placeholder='you@email.com'
+          style={styles.input}
+          onChangeText={text => setEmail(text)}
+        />
+      </Item>
+      <Button rounded style={styles.button}>
+        <Text>Send Report</Text>
+        <Ionicons name='ios-send' style={styles.icon} size={26} color='white' />
+      </Button>
+      <Button
+        rounded
+        style={styles.button}
+        onPress={() => navigation.push('Welcome')}
+      >
+        <Text>New Checkup</Text>
+        <MaterialCommunityIcons
+          name='stethoscope'
+          style={styles.icon}
+          size={26}
+          color='white'
+        />
+      </Button>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingBottom: height * 0.075
+  },
+  contentContainer: {
+    alignItems: 'center',
+    flex: 1
+  },
+  input: {
+    fontSize: 16,
+    width: '80%'
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    paddingTop: height * 0.025
+  },
+  button: {
+    marginTop: height * 0.02,
+    width: '50%'
+  },
+  icon: {
+    marginRight: height * 0.02
+  },
+  report: {
+    alignSelf: 'center',
+    borderWidth: 0,
+    borderRadius: 50,
+    height: height * 0.2,
+    marginBottom: height * 0.01,
+    marginTop: height * 0.04,
+    width: height * 0.2
   }
-})
+});
