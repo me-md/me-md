abd0fee47380dc38a1f421036397903a400814f0
posi7790 <posi7790@colorado.edu>
2020-01-08 13:41:32 -0700

    Clean up files

--- utils/apiKeys/apiKeys.js
+++
@@ -1,9 +0,0 @@
-import { REACT_APP_ID, REACT_APP_KEY, MAPQUEST_KEY } from 'react-native-dotenv';
-
-const keys = TRAVIS ? {} : {
-  REACT_APP_ID,
-  REACT_APP_KEY,
-  MAPQUEST_KEY
-};
-
-export default keys
