06d570b7445237ba73b5a7dd476b2b3ad01dae5b
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-08 17:38:57 -0700

    Implement logo in Header component and splash screen logo

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-
+import { Dimensions, Image, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const height = Dimensions.get('window').height;
 
-export function Header(props) {
+export function Header() {
   return (
-    <LinearGradient
-      colors={['#004EFF', '#88CCF1']}
-      style={styles.header}
-    ></LinearGradient>
+    <LinearGradient colors={['#004EFF', '#88CCF1']} style={styles.header}>
+      <Image
+        source={require('../assets/images/name.png')}
+        style={styles.icon}
+      />
+    </LinearGradient>
   );
-
 }
 
 const styles = StyleSheet.create({
   header: {
-    height: height * 0.15,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: height * 0.15
+  },
+  icon: {
+    alignSelf: 'center',
+    borderWidth: 0,
+    height: height * 0.1,
+    marginBottom: height * 0.01,
+    marginTop: height * 0.045,
+    width: height * 0.1
   }
-})
+});
